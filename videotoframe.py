a63ff3a879df8a8f545e7038d23292836fac9d58
Joshan <joathenz@gmail.com>
2018-10-15 17:26:37 +0530

    Add files via upload

---
+++ videotoframe.py
@@ -0,0 +1,9 @@
+import cv2
+vidcap = cv2.VideoCapture('./train/pedes_train.avi')
+success,image = vidcap.read()
+count = 0;
+while success:
+	success,image = vidcap.read()
+	cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+	if cv2.waitKey(10) == 27:                     # exit if Escape is hit    	break
+	   count += 1
